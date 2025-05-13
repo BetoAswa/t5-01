5b8ae3e5426b9241ede1bcec23f8f74a3b6cc62b
Juan Arroyo <juanparroyo296@gmail.com>
2025-05-13 13:59:39 -0600

    Primer commit

---
+++ app/index.tsx
@@ -0,0 +1,112 @@
+import { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button, FlatList, TouchableOpacity } from "react-native";
+
+import * as Location from "expo-location";
+import * as Clipboard from "expo-clipboard"
+import { CameraView, CameraType, useCameraPermissions, BarcodeScanningResult } from "expo-camera";
+import { connectDb } from "../src/database";
+
+
+interface ScannerCode {
+    code: BarcodeScanningResult,
+    location: Location.LocationObject
+}
+
+export default () => {
+    const [location, setLocation] = useState<Location.LocationObject | null>(null);
+    const [errorMsg, setErrorMsg] = useState<string | null>(null);
+    const [facing, setFacing] = useState<CameraType>("back");
+    const [permission, requestPermission] = useCameraPermissions();
+    const [scannedCodes, setScannedCodes] = useState<ScannerCode[]>([]);
+
+    useEffect(() => {
+        async function getCurrentLocation() {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        }
+        getCurrentLocation();
+    }, []);
+
+    if (!permission) {
+        return <View />;
+    }
+    if (!permission.granted) {
+        return (
+            <View >
+                <Text>Camera permission is required to use this app.</Text>
+                <Button title="Grant Permission" onPress={requestPermission} />
+            </View>
+        );
+    }
+
+    let text = 'Waiting..';
+    if (errorMsg) {
+        text = errorMsg;
+    }
+    else if (location) {
+        text = JSON.stringify(location);
+    }
+
+    const onBarcodeScanned = async function (result: BarcodeScanningResult) {
+        if (window) {
+            window.alert(result.data)
+        } else {
+            alert(result.data)
+        }
+        setScannedCodes([{code: result, location: location!}, ...scannedCodes]);
+        const db = await connectDb();
+        await db.insertarCodigo(result.data, result.type);
+        console.log(await db.consultarCodigos())
+    }
+
+    const ScannedItem = function ({ item }: { item: ScannerCode }) {
+      const onCopyPress = function(){
+        Clipboard.setStringAsync(item.code.data);
+      };
+        return (
+            <View>
+                <Text>{item.code.data}</Text>
+                <TouchableOpacity>
+                  <Text>Copiar</Text>
+                </TouchableOpacity>
+                { item.location && (
+                    <>
+                     <Text>{item.location?.timestamp}</Text>
+                     <Text>Lat {item.location?.coords.latitude}, Long: {item.location?.coords.longitude}</Text>
+                    </>
+                )}
+            </View>
+        )
+    }
+    return (
+        <View >
+            <Text >GPS: {text}</Text>
+            <CameraView facing={facing} style={styles.CameraView}
+                barcodeScannerSettings={{
+                    barcodeTypes: ['qr', "code128", "datamatrix", "aztec"]
+                }}
+                onBarcodeScanned={onBarcodeScanned}
+            />
+            <FlatList data={scannedCodes}
+                keyExtractor={(item) => item.location?.timestamp.toFixed(0)}
+                renderItem={ScannedItem}
+            />
+        </View>
+    )
+
+}
+
+
+const styles = StyleSheet.create({
+    CameraView: {
+        width: "100%",
+        minHeight: 240,
+    }
+
+});
